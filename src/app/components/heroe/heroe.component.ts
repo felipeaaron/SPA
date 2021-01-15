ff18ff6c6314913e55a7081ea90354114cea74df
Felipe Aaron <felipe.aaron@levelup.cl>
2021-01-14 21:12:00 -0300

    SPA Heroes angular

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AppComponent } from '../../app.component';
+
+import { HeroesService } from '../../servicios/heroes.service';
+
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent  {
+heroe:any={};
 
-  constructor() { }
+  
+constructor( private activateRoute: ActivatedRoute,
+             private _heroesService: HeroesService){
 
-  ngOnInit(): void {
-  }
+this.activateRoute.params.subscribe(params => {
+  console.log( params['id']);
+  this.heroe= this._heroesService.getHeroe(params['id']);
+
+
+
+})
+}
+  
 
 }
--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -66,6 +66,9 @@ export class HeroesService {
     getHeroes(){
         return this.heroes;
     }
+    getHeroe( idx: string){
+      return this.heroes[idx];
+    }
 }
    export interface Heroe{
         nombre: String;
